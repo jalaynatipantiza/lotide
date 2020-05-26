66b1a1cac69c1a94f8cf0c3341370bad882fb0a7
jalayna <jalaynatipantiza@gmail.com>
2020-05-26 03:37:12 +0000

    Initial assert function

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ n" +  actual + " === " + expected);
+  } else {
+    console.log("❌❌❌ " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 5);
