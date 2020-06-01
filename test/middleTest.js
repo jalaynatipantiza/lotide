dd26988234189a41b12feee5b08959bdad82eff5
jalayna <jalaynatipantiza@gmail.com>
2020-06-01 21:06:02 +0000

    deleted comments

--- test/middleTest.js
+++ test/middleTest.js
@@ -13,6 +13,3 @@ describe("#middle", () => {
   });
 });
 
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1,2,3,4,5]), [3]);
-// assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
