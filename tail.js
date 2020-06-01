dfa283d001a1a7128870a3452b54dab0489d73af
jalayna <jalaynatipantiza@gmail.com>
2020-06-01 14:49:03 +0000

    modularized and refactored code

--- tail.js
+++ tail.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
+
 const tail = function(array) {
   let newArray = [];
   for (let i = 1; i < array.length; i++) {
@@ -13,7 +8,4 @@ const tail = function(array) {
   return newArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
