eeea3d5e8c26a95737fb62d91c233c7eafd54e3a
jalayna <jalaynatipantiza@gmail.com>
2020-06-01 16:02:10 +0000

    modularized and refactored code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,5 @@
+const eqArrays = require('./eqArrays')
 
-//compares two arrays and makes sure they are a perfect match
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 const assertArraysEqual = function(arrayA, arrayB) {
   if (eqArrays(arrayA, arrayB)) {
     console.log(`✅✅✅ [${arrayA}] === [${arrayB}]`);
@@ -18,7 +7,6 @@ const assertArraysEqual = function(arrayA, arrayB) {
     console.log(`❌❌❌ [${arrayA}] !== [${arrayB}]`);
   }
 };
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+
+module.exports = assertArraysEqual;
+
